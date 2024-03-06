10bcb5c6fb0ec09d3c262080e0d06ee28e206e76
Rustam-Ulaevich <praporglamur@gmail.com>
2024-03-06 23:36:35 +0500

    Function

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print(2+2)
+age = input('Enter your age: ')
+print(age)
+
+print(True)
+
+# Function
+
+def my_func(a,b):
+  a = a + 1
+  c = a + b
+  return c
+
+print(my_func(1, 2))
+
+print(type(2))
+
+name = 'Rustam'
+nick = 'Super'
+
+print(name.upper())
+print(name  + " " +  nick)
+
+print(1,'Fuck',False)
+
+# print(dir(__builtins__))
